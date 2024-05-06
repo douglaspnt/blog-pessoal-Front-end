9dea2ad1636640c2c22edfd3f096e4e2415be4dd
douglaspnt <161038707+douglaspnt@users.noreply.github.com>
2024-05-06 16:57:44 -0300

    ate o cookbook 23

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import Navbar from './componentes/NavBar/NavBar';
+import Footer from './componentes/Footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
+import Cadastro from './paginas/cadastro/Cadastro';
+import Home from './paginas/home/Home';
+import { AuthProvider } from './contexts/AuthContext';
+import ListaTemas from './componentes/temas/listaTemas/ListaTemas';
+import FormularioTema from './componentes/temas/formularioTema/FormularioTema';
+import DeletarTema from './componentes/temas/deletarTema/DeletarTema';
+
+
+function App() {
+  return (
+    <>
+    <AuthProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+        </AuthProvider>
+    </>
+  );
+}
+export default App;
